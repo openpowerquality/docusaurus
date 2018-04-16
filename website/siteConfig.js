019790683c947bea79dbb293421eaaa4a51cfa6c
Philip Johnson <johnson@hawaii.edu>
2018-04-16 07:36:10 -1000

    checkpoint.

---
+++ website/siteConfig.js
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// See https://docusaurus.io/docs/site-config.html for all the possible
+// site configuration options.
+
+/* List of projects/orgs using your project for the users page */
+const users = [
+  {
+    caption: 'User1',
+    image: '/test-site/img/docusaurus.svg',
+    infoLink: 'https://www.facebook.com',
+    pinned: true,
+  },
+];
+
+const siteConfig = {
+  title: 'Open Power Quality' /* title for your website */,
+  tagline: 'Open Power Quality develops hardware and software for low-cost distributed power quality data collection, analysis, and visualization.',
+  url: 'https://openpowerquality.org' /* your website url */,
+  baseUrl: '/' /* base url for your project */,
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  // Used for publishing and more
+  projectName: 'docusaurus',
+  organizationName: 'openpowerquality',
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    {doc: 'doc1', label: 'Docs'},
+    {doc: 'doc4', label: 'API'},
+    {page: 'help', label: 'Help'},
+    {blog: true, label: 'Blog'},
+  ],
+
+  // If you have users set above, you add it here:
+  users,
+
+  /* path to images for header/footer */
+  headerIcon: 'img/opqlogo.png',
+  footerIcon: 'img/opqlogo.png',
+  favicon: 'img/opq.ico',
+
+  /* colors for website */
+  colors: {
+    primaryColor: "rgb(34, 34, 34)",
+    secondaryColor: "#05A5D1",
+    tintColor: "#005068",
+    backgroundColor: "#f5fcff"
+  },
+
+  /* custom fonts for website */
+  /*fonts: {
+    myFont: [
+      "Times New Roman",
+      "Serif"
+    ],
+    myOtherFont: [
+      "-apple-system",
+      "system-ui"
+    ]
+  },*/
+
+  // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
+  copyright:
+    'Copyright © ' +
+    new Date().getFullYear() +
+    ' Your Name or Your Company Name',
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks
+    theme: 'default',
+  },
+
+  scripts: ['https://buttons.github.io/buttons.js'],
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  //   repoUrl: 'https://github.com/facebook/test-site',
+
+  /* On page navigation for the current documentation page */
+  // onPageNav: 'separate',
+};
+
+module.exports = siteConfig;
