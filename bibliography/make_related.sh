b392a1c14f4bf3f02eb77711480ed050e8ce70e7
Philip Johnson <johnson@hawaii.edu>
2018-05-20 08:03:39 -1000

    Add citations on renewable energy

--- bibliography/make_related.sh
+++ bibliography/make_related.sh
@@ -6,5 +6,6 @@ openout_any=a bibtex2html -dl -nodoc -nobibsource -nokeys -nokeywords -nofooter 
 openout_any=a bibtex2html -dl -nodoc -nobibsource -nokeys -nokeywords -nofooter hawaii.bib
 openout_any=a bibtex2html -dl -nodoc -nobibsource -nokeys -nokeywords -nofooter monitoring.bib
 openout_any=a bibtex2html -dl -nodoc -nobibsource -nokeys -nokeywords -nofooter opq.bib
+openout_any=a bibtex2html -dl -nodoc -nobibsource -nokeys -nokeywords -nofooter renewables.bib
 openout_any=a bibtex2html -dl -nodoc -nobibsource -nokeys -nokeywords -nofooter standards.bib
-cat header.txt opq-header.txt opq.html related-work-header.txt general-header.txt general.html standards-header.txt standards.html case-studies-header.txt case-studies.html classification-header.txt classification.html economics-header.txt economics.html hawaii-header.txt hawaii.html monitoring-header.txt monitoring.html > ../docs/intro-bibliography.md
+cat header.txt opq-header.txt opq.html related-work-header.txt general-header.txt general.html standards-header.txt standards.html case-studies-header.txt case-studies.html classification-header.txt classification.html economics-header.txt economics.html hawaii-header.txt hawaii.html monitoring-header.txt monitoring.html renewables-header.txt renewables.html > ../docs/intro-bibliography.md
